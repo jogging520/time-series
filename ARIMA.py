71c7a70e21cb75a2f9b93deb36a01a26cce8d2d0
jogging520 <446734796@qq.com>
2017-12-16 23:30:56 +0800

    need to fix end to end

--- ARIMA.py
+++ ARIMA.py
@@ -1,11 +1,41 @@
-import pandas as pd
 import numpy as np
-from pandas_datareader.data import DataReader
+from math import sqrt
+import pandas as pd
+import pyflux as pf
 from datetime import datetime
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+#matplotlib inline
+#存在日期转换的问题----------------------------------
+def parser(x):
+	return datetime.strptime(x, '%Y/%m/%d')
 
-a = DataReader('JPM',  'yahoo', datetime(2006,6,1), datetime(2016,6,1))
-a_returns = pd.DataFrame(np.diff(np.log(a['Adj Close'].values)))
-a_returns.index = a.index.values[1:a.index.values.shape[0]]
-a_returns.columns = ["JPM Returns"]
+# data = pd.read_csv('sp500.csv',header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# print(data.head())
+# # data.index = data['time'].values
+# plt.figure(figsize=(15,5))
+# data.plot()
+# plt.show()
 
-a_returns.head()
+series = pd.read_csv('sp500.csv')
+# plt.ylabel('price')
+# plt.title('time')
+# series.plot()
+# plt.show()
+
+model = pf.ARIMA(data=series, ar=4, ma=4, target='price', family=pf.Normal())
+
+x = model.fit("MLE")
+# x.summary()
+# #
+# model.plot_fit(figsize=(15,10))
+# model.plot_predict_is(h=20, figsize=(15,5))
+
+yhat = model.predict_is(20)
+y = series['price'].values[-20:]
+
+print(y)
+input('enter')
+# calculate RMSE
+rmse = sqrt(mean_squared_error(y, yhat))
+print('Test RMSE: %.3f' % rmse)
---
+++ seq2seq.py
@@ -0,0 +1,157 @@
+from math import sqrt
+from numpy import concatenate
+from numpy import array
+from numpy import argmax
+from numpy import array_equal
+from keras.utils import to_categorical
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.models import Sequential
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.metrics import mean_squared_error
+
+
+
+# convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+def plot_results(predicted_data, true_data):
+    fig = pyplot.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    pyplot.plot(predicted_data, label='Prediction')
+    pyplot.legend()
+    pyplot.show()
+
+
+# load dataset
+dataset = read_csv('sp500.csv', header=0, index_col=0)
+print(dataset.head())
+values = dataset.values
+print(values)
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled,7,7)
+
+print(reframed.head())
+
+input('enter')
+# split into train and test sets
+values = reframed.values
+n_train_days = 3000
+train = values[:n_train_days, :]
+test = values[n_train_days:, :]
+# split into input and outputs
+train_X, train_y = train[:, :-7], train[:,-7:]
+test_X, test_y = test[:, :-7], test[:, -7:]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+input('enter')
+
+
+# returns train, inference_encoder and inference_decoder models
+def define_models(n_input, n_output, n_units):
+	# define training encoder
+	encoder_inputs = Input(shape=(None, n_input))
+	encoder = LSTM(n_units, return_state=True)
+	encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+	encoder_states = [state_h, state_c]
+	# define training decoder
+	decoder_inputs = Input(shape=(None, n_output))
+	decoder_lstm = LSTM(n_units, return_sequences=True, return_state=True)
+	decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+	decoder_dense = Dense(n_output, activation='softmax')
+	decoder_outputs = decoder_dense(decoder_outputs)
+	model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+	# define inference encoder
+	encoder_model = Model(encoder_inputs, encoder_states)
+	# define inference decoder
+	decoder_state_input_h = Input(shape=(n_units,))
+	decoder_state_input_c = Input(shape=(n_units,))
+	decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+	decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+	decoder_states = [state_h, state_c]
+	decoder_outputs = decoder_dense(decoder_outputs)
+	decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+	# return all models
+	return model, encoder_model, decoder_model
+
+# generate target given source sequence
+def predict_sequence(infenc, infdec, source, n_steps, cardinality):
+	# encode
+	state = infenc.predict(source)
+	# start of sequence input
+	target_seq = array([0.0 for _ in range(cardinality)]).reshape(1, 1, cardinality)
+	# collect predictions
+	output = list()
+	for t in range(n_steps):
+		# predict next char
+		yhat, h, c = infdec.predict([target_seq] + state)
+		# store prediction
+		output.append(yhat[0,0,:])
+		# update state
+		state = [h, c]
+		# update target sequence
+		target_seq = yhat
+	return array(output)
+
+
+# configure problem
+n_features = 50 + 1
+n_steps_in = 7
+n_steps_out = 7
+# define model
+train, infenc, infdec = define_models(n_features, n_features, 128)
+train.compile(optimizer='adam', loss='mae', metrics=['acc'])
+X1, y= train_X, train_y
+print(X1.shape,y.shape)
+input('enter')
+# train model
+train.fit([X1], y, epochs=1)
+# evaluate LSTM
+total, correct = 100, 0
+for _ in range(total):
+	X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
+	target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
+	if array_equal(one_hot_decode(y[0]), one_hot_decode(target)):
+		correct += 1
+print('Accuracy: %.2f%%' % (float(correct)/float(total)*100.0))
+# spot check some examples
+for _ in range(10):
+	X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
+	target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
+	print('X=%s y=%s, yhat=%s' % (one_hot_decode(X1[0]), one_hot_decode(y[0]), one_hot_decode(target)))
