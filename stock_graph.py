530e78d21145c67f358f5c9e7f7674e9e2f0b2a2
jogging520 <446734796@qq.com>
2018-03-13 23:23:03 +0800

    plot fighure

--- stock_graph.py
+++ stock_graph.py
@@ -6,22 +6,18 @@ import matplotlib.pyplot as plt
 def parser(x):
 	return datetime.strptime(x, '%m/%d/%Y')
 
-data = pd.read_csv('sp500_data.csv',header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-print(data.head())
+data = pd.read_csv('sp500_data.csv',header=0, parse_dates=[0], squeeze=True, date_parser=parser)
+#print(data['trade_date'])
 # data.index = data['time'].values
-plt.figure(figsize=(10,5))
-
+#
+# plt.figure(figsize=(10,5))
 # data.index = data['time'].values
 plt.ylabel('S&P500_index')
 plt.xlabel('Trade_date')
-plt.figure(figsize=(10,5))
-plt.plot(data)
 plt.title('S&P 500 index series')
+plt.plot(data['value'])
+plt.grid()
+plt.annotate(u"This is a zhushi", xy = (0, 1), xytext = (1, 50),\
+             arrowprops = dict(facecolor = "r", headlength = 10, headwidth = 30, width = 20))
+plt.vlines(2983, 600, 2250, colors="r", linestyles="dashed")
 plt.show()
-
-
-# series = pd.read_csv('sp500.csv')
-# plt.ylabel('price')
-# plt.title('time')
-# series.plot()
-# plt.show()
